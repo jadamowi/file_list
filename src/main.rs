5d681f8fcf0a2a13135fb0cd24f421849ac78512
Jakub Adamowicz <jakubadamowicz1@gmail.com>
2021-10-31 21:55:43 +0100

    read current dir

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
+use glob::glob;
 use std::env;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{Write, BufReader, BufRead, Error};
 
 fn main() -> Result<(), Error> {
-    let current_dir = env::current_dir().unwrap();
+    let current_dir: str = env::current_dir().unwrap();
+    let mut file = OpenOptions::new().append(true).open("files.txt");
 
-    let path = "files.txt";
-
-    let mut output = File::create(path)?;
-    write!(output, "{}", current_dir.display())?;
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    for line in buffered.lines() {
-        println!("{}", line?);
+    for entry in glob(&current_dir).expect("Failed to read current directory") {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
     }
 
     Ok(())
