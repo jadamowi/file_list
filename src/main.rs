32865b022fcf0686364e2ee33c72891309d94c09
Jakub Adamowicz <jakubadamowicz1@gmail.com>
2021-11-27 17:49:50 +0100

    correct variable names

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use std::io::{Error, stdin};
 
 
 fn main() -> Result<(), Error> {
-    let mut current_dir = String::new();
-    stdin().read_line(&mut current_dir).expect("Did not enter a correct string");
-    let trimmed_dir = &current_dir.trim_end().to_string();
-    let current_dir_look = format!("{}\\**", &trimmed_dir);
+    let mut current_dir_input = String::new();
+    stdin().read_line(&mut current_dir_input).expect("Did not enter a correct string");
+    let trimmed_dir = &current_dir_input.trim_end().to_string();
+    let current_dir_to_loop = format!("{}\\**", &trimmed_dir);
     
     // let path_file = OpenOptions::new().append(true).open("files.txt");
 
-    for entry in glob(&current_dir_look).expect("Failed to read current directory") {
+    for entry in glob(&current_dir_to_loop).expect("Failed to read current directory") {
         match entry {
             Ok(path) => println!("{:?}", path.display()),
             Err(e) => println!("{:?}", e),
