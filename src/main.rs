5cf01af542be659f8dc4d57752f0090b1aae4671
Jakub Adamowicz <jakubadamowicz1@gmail.com>
2021-11-27 21:03:44 +0100

    add vector container

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use glob::glob;
+use std::fs;
 use std::io::{Error, stdin};
-//use std::env;
-//use std::fs::{File, OpenOptions};
-//use std::io::{Write, Error, stdin, stdout,};
 
 
 fn main() -> Result<(), Error> {
     let mut current_dir_input = String::new();
     stdin().read_line(&mut current_dir_input).expect("Did not enter a correct string");
     let trimmed_dir = &current_dir_input.trim_end().to_string();
-    let current_dir_to_loop = format!("{}\\**", &trimmed_dir);
-    
-    // let path_file = OpenOptions::new().append(true).open("files.txt");
+    let current_dir_to_loop = format!("{}\\**\\*", &trimmed_dir);
+
+    let mut paths:Vec<std::path::PathBuf>  = Vec::new();
 
     for entry in glob(&current_dir_to_loop).expect("Failed to read current directory") {
         match entry {
-            Ok(path) => println!("{:?}", path.display()),
+            Ok(path) => if check_type(&path) {paths.push(path)},
             Err(e) => println!("{:?}", e),
         }
     }
+    
+    println!("{:?}", paths);
 
     Ok(())
 }
 
+
+fn check_type(file_buf:&std::path::PathBuf) -> bool {
+    let metadata = fs::metadata(&file_buf).unwrap();
+    let file_type = metadata.file_type();
+    file_type.is_file()
+}
 
