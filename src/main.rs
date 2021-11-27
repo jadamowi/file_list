b44da4e3f0c9d5737a25d289621ece75f8bdca2b
Jakub Adamowicz <jakubadamowicz1@gmail.com>
2021-11-27 17:45:56 +0100

    finish printing to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use glob::glob;
-use std::env;
-use std::fs::{File, OpenOptions};
-use std::io::{Write, Error, stdin, stdout,};
+use std::io::{Error, stdin};
+//use std::env;
+//use std::fs::{File, OpenOptions};
+//use std::io::{Write, Error, stdin, stdout,};
 
 
 fn main() -> Result<(), Error> {
     let mut current_dir = String::new();
     stdin().read_line(&mut current_dir).expect("Did not enter a correct string");
-    let mut file = OpenOptions::new().append(true).open("files.txt");
+    let trimmed_dir = &current_dir.trim_end().to_string();
+    let current_dir_look = format!("{}\\**", &trimmed_dir);
+    
+    // let path_file = OpenOptions::new().append(true).open("files.txt");
 
-    for entry in glob(&current_dir).expect("Failed to read current directory") {
+    for entry in glob(&current_dir_look).expect("Failed to read current directory") {
         match entry {
             Ok(path) => println!("{:?}", path.display()),
             Err(e) => println!("{:?}", e),
